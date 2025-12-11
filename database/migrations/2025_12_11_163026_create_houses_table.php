f18276022df7d9763a143229181bed851849d292
erzsebet <loskutnikovalza123@gmail.com>
2025-12-11 22:16:09 +0500

    Создание миграции houses

---
+++ database/migrations/2025_12_11_163026_create_houses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHousesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('houses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 255);
+            $table->string('image_url', 255);
+            $table->string('text', 255);
+            $table->string('modaltext', 255);
+            $table->dateTime('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('houses');
+    }
+}
