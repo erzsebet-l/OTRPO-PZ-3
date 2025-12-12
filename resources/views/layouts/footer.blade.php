c28523d4f884ae94a0ed9a5b422da07e552519d7
erzsebet <loskutnikovalza123@gmail.com>
2025-12-13 03:07:10 +0500

    шаблон footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,18 @@
+<footer class="bottom-bar text-light border-top border-secondary py-3 mt-4">
+  <div class="container-fluid">
+    <div class="row justify-content-between align-items-center">
+      <div class="col-12 col-md-auto text-center text-md-start mb-2 mb-md-0">
+        <div class="author fw-bold">Лоскутникова Елизавета</div>
+      </div>
+
+      <div class="col-12 col-md-auto d-flex justify-content-center justify-content-md-end gap-3">
+        <a href="https://github.com/erzsebet-l" target="_blank" class="social-link">
+          <img src="https://avatars.mds.yandex.net/i?id=7fecdca8b83644e824eaf54db6dc2602bc160548-5233303-images-thumbs&n=13" alt="Github" width="30">
+        </a>
+        <a href="https://messenger.360.yandex.ru/p/822a6f01-f333-9446-4ee1-9953855d7e28?utm_source=invite" target="_blank" class="social-link">
+          <img src="https://www.iguides.ru/upload/iblock/463/463bd7676d638841f0962c35e35be182.png" alt="Яндекс Мессенджер" width="30">
+        </a>
+      </div>
+    </div>
+  </div>
+</footer>
